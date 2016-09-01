f6a090cd63c898359ca7c144078224d3c9e52309
Taptigin <trpk30273@gmail.com>
2016-09-01 23:28:20 +0400

    разрабатываю фронт

--- src/main/java/com/mycomp/MainBean.java
+++ src/main/java/com/mycomp/MainBean.java
@@ -1,6 +1,7 @@
 package com.mycomp;
 
 import com.mycomp.models.Order;
+import com.mycomp.models.OrderDetails;
 import com.mycomp.service.OrderService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -16,41 +17,21 @@ import java.util.List;
  * Created by Александр on 01.09.2016.
  */
 @ManagedBean(name = "mainBean")
-@RequestScoped
-//@SessionScoped
+@SessionScoped
 public class MainBean {
 
     private ApplicationContext context = new ClassPathXmlApplicationContext("SpringContext.xml");
     private OrderService service = (OrderService) context.getBean("storageService");
 
-    private String inputText;
-    String qqq = service.getOrders().get(0).getCustomerName();
+
 
     public List<Order> getOrders(){
         return service.getOrders();
     }
 
-    public String getInputText() {
-        System.out.println(service.getOrders().get(0).getCustomerName());
-        return inputText;
+    public List<OrderDetails> getDetails(long orderId){
+        return service.getOrderDetails(orderId);
     }
 
-    public String getQqq() {
-        System.out.println(service.getOrders().get(0).getCustomerName());
-        return qqq;
-    }
 
-    public void setInputText(String inputText) {
-        this.inputText = inputText;
-    }
-
-    public void showMessage() {
-        FacesMessage message = new FacesMessage("Заголовок", "Частичное обновление страницы");
-        message.setSeverity(FacesMessage.SEVERITY_INFO); //как выглядит окошко с сообщением
-        FacesContext.getCurrentInstance().addMessage(null, message);
-
-        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,"Всплывашка", "GrowlMessage"));
-
-        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL,"Значение", inputText));
-    }
 }
